cbc6d02d35624b252dc4da20a35f4d46dc7b92d4
Ubuntu <ubuntu@ip-172-31-13-122.ap-south-2.compute.internal>
2026-06-01 14:53:33 +0000

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>React Microservice 1</h1>
+      <h1>Arun kumar demo1</h1>
       <p>Running inside Docker Container</p>
     </div>
   );
