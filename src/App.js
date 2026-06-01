d7074b3139b0a27ac97e4bb2ad384288b77788cf
Ubuntu <ubuntu@ip-172-31-13-122.ap-south-2.compute.internal>
2026-06-01 15:45:53 +0000

    last

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Arun kumar demo Done</h1>
+      <h1>CI/CD Implemented</h1>
       <p>Running inside k8 Container</p>
     </div>
   );
